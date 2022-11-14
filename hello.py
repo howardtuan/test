5a2b54f13e7564cfd1bf04174fbd0252cb043c2a
段浩恩 Howard Tuan <howardtuan@gmail.com>
2022-11-14 22:52:42 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,4 @@
 print("Hello world")
 for i in range(3):
     print('what')
-    print('what22')
-    print('what33')
-    print('what')
-    print('what22')
-    print('what33')
-    print('what')
-    print('what22')
-    print('what33')
 #kuo
