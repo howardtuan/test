8ff4f84e0a4aad2d68bd4ee51fa74215e8d4b0ab
Howard <howardtuan@gmail.com>
2022-11-14 22:12:42 +0800

    more

--- hello.py
+++ hello.py
@@ -3,4 +3,10 @@ for i in range(3):
     print('what')
     print('what22')
     print('what33')
+    print('what')
+    print('what22')
+    print('what33')
+    print('what')
+    print('what22')
+    print('what33')
 #kuo
